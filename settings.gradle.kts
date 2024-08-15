b85b162a342b2c2f9c141104b9b9cccc961793c3
Syz <hyeoski+zircta@gmail.com>
2024-08-15 01:12:18 -0400

    Add 1.12.2 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,9 @@ rootProject.buildFileName = "root.gradle.kts"
 // Adds all of our build target versions to the classpath if we need to add version-specific code.
 listOf(
     "1.8.9-forge", // Update this if you want to remove/add a version, along with `build.gradle.kts` and `root.gradle.kts`.
-    "1.8.9-fabric"
+    "1.8.9-fabric",
+    "1.12.2-forge",
+    "1.12.2-fabric"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
