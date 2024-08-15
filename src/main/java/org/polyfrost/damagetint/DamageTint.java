3261d980b03faad0c63dc63e0985f9664127cd01
Syz <hyeoski+zircta@gmail.com>
2024-08-15 01:03:16 -0400

    Add legacy fabric support

--- src/main/java/org/polyfrost/damagetint/DamageTint.java
+++ src/main/java/org/polyfrost/damagetint/DamageTint.java
@@ -1,18 +1,14 @@
 package org.polyfrost.damagetint;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import org.polyfrost.damagetint.command.DamageTintCommand;
 import org.polyfrost.damagetint.config.DamageTintConfig;
 import org.polyfrost.oneconfig.api.commands.v1.CommandManager;
 
-@Mod(name = DamageTint.NAME, version = DamageTint.VER, modid = DamageTint.ID)
 public class DamageTint {
     public static final String NAME = "@NAME@", VER = "@VER@", ID = "@ID@";
     public static DamageTintConfig config;
 
-    @Mod.EventHandler
-    protected void onInitialization(FMLInitializationEvent event) {
+    public static void initialize() {
         config = new DamageTintConfig();
         CommandManager.registerCommand(new DamageTintCommand());
     }
