fe1369048de6865e1d75fde2b3cd171e979a0a9c
Syz <hyeoski+zircta@gmail.com>
2024-08-15 01:12:18 -0400

    Add 1.12.2 support

--- root.gradle.kts
+++ root.gradle.kts
@@ -7,8 +7,13 @@ plugins {
 preprocess {
     strictExtraMappings.set(true)
     val fabric10809 = createNode("1.8.9-fabric", 10809, "yarn")
+    val fabric11202 = createNode("1.12.2-fabric", 11202, "srg")
 
     val forge10809 = createNode("1.8.9-forge", 10809, "srg")
+    val forge11202 = createNode("1.12.2-forge", 11202, "srg")
 
     fabric10809.link(forge10809)
+    fabric11202.link(fabric10809)
+
+    forge11202.link(fabric11202)
 }
