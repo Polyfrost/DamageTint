efec0770b9a59e45f804c7131294f869a8032b07
Deftu <me@deftu.dev>
2025-10-02 07:17:15 +0200

    port finished

--- src/main/java/org/polyfrost/damagetint/client/DamageTintClient.java
+++ src/main/java/org/polyfrost/damagetint/client/DamageTintClient.java
@@ -1,6 +1,8 @@
 package org.polyfrost.damagetint.client;
 
-import org.polyfrost.oneconfig.api.commands.v1.CommandManager;
+import dev.deftu.omnicore.api.client.commands.OmniClientCommands;
+import org.polyfrost.damagetint.DamageTintConstants;
+import org.polyfrost.oneconfig.utils.v1.dsl.ScreensKt;
 
 //#if MC >= 1.16.5
 //$$ import org.polyfrost.oneconfig.api.event.v1.EventManager;
@@ -8,16 +10,16 @@ import org.polyfrost.oneconfig.api.commands.v1.CommandManager;
 //#endif
 
 public class DamageTintClient {
-
     public static final DamageTintClient INSTANCE = new DamageTintClient();
 
     public void initialize() {
         DamageTintConfig.INSTANCE.preload();
-        CommandManager.register(new DamageTintCommand());
+        OmniClientCommands.register(OmniClientCommands.literal(DamageTintConstants.ID).executes(ctx -> {
+            return ctx.getSource().openScreen(ScreensKt.createScreen(DamageTintConfig.INSTANCE));
+        }));
 
         //#if MC >= 1.16.5
         //$$ EventManager.register(InitializationEvent.class, () -> DamageTintConfig.updateOverlayColor(DamageTintConfig.color));
         //#endif
     }
-
 }
--- src/main/java/org/polyfrost/damagetint/client/utils/OverlayModifier.java
+++
@@ -1,7 +0,0 @@
-package org.polyfrost.damagetint.client.utils;
-
-public interface OverlayModifier {
-
-    void setOverlayColor(int argb);
-
-}
