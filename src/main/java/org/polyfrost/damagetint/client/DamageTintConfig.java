c5df5e230e3e7c66ab8290b9df4cc4c4df3c094b
Tryfle <xinaout410@gmail.com>
2026-01-15 22:06:40 -0500

    port to stonecutter

--- src/main/java/org/polyfrost/damagetint/client/DamageTintConfig.java
+++ src/main/java/org/polyfrost/damagetint/client/DamageTintConfig.java
@@ -1,6 +1,11 @@
 package org.polyfrost.damagetint.client;
 
+import dev.deftu.omnicore.api.client.OmniClient;
+import dev.deftu.omnicore.api.client.OmniClientRuntime;
+import dev.deftu.omnicore.api.color.OmniColor;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import org.polyfrost.damagetint.DamageTintConstants;
+import org.polyfrost.damagetint.client.utils.OverlayModifier;
 import org.polyfrost.oneconfig.api.config.v1.Config;
 import org.polyfrost.oneconfig.api.config.v1.annotations.Button;
 import org.polyfrost.oneconfig.api.config.v1.annotations.Color;
@@ -8,15 +13,8 @@ import org.polyfrost.oneconfig.api.config.v1.annotations.Switch;
 import org.polyfrost.polyui.color.ColorUtils;
 import org.polyfrost.polyui.color.PolyColor;
 
-//#if MC >= 1.16.5
-//$$ import dev.deftu.omnicore.api.color.OmniColor;
-//$$ import dev.deftu.omnicore.api.client.OmniClient;
-//$$ import dev.deftu.omnicore.api.client.OmniClientRuntime;
-//$$ import net.minecraft.client.renderer.texture.OverlayTexture;
-//$$ import org.polyfrost.damagetint.client.utils.OverlayModifier;
-//#endif
-
 public class DamageTintConfig extends Config {
+
     public static final DamageTintConfig INSTANCE = new DamageTintConfig();
 
     @Switch(title = "Enable Damage Tint")
@@ -24,7 +22,7 @@ public class DamageTintConfig extends Config {
 
     private static final int defaultColor = 1291780096;
 
-    @Color(title = "Damage Tint Colour")
+    @Color(title = "Damage Tint Color")
     public static PolyColor color = ColorUtils.toColor(defaultColor);
 
     @Button(title = "Reset Damage Tint", text = "Reset Color")
@@ -33,40 +31,27 @@ public class DamageTintConfig extends Config {
         save();
     }
 
-    //#if MC >= 1.16.5
-    //$$ @Button(title = "Fix Damage Tint Colour", text = "Fix Color")
-    //$$ private void fixColor() {
-    //$$     updateOverlayColor(color);
-    //$$ }
-    //#endif
-
     @Switch(title = "Fade Out Damage Tint")
     public static boolean fade = false;
 
     public DamageTintConfig() {
         super("damagetint.json", "/assets/damagetint/damagetint_dark.svg", DamageTintConstants.NAME, Category.QOL);
         save();
-
-        //#if MC >= 1.16.5
-        //$$ addCallback("color", () -> {
-        //$$     updateOverlayColor(color);
-        //$$ });
-        //#endif
     }
 
-    //#if MC >= 1.16.5
-    //$$ public static void updateOverlayColor(PolyColor newColor) {
-    //$$     OmniClientRuntime.runOnMain(() -> {
-    //$$         int red = newColor.red();
-    //$$         int green = newColor.green();
-    //$$         int blue = newColor.blue();
-    //$$         // Alpha is flipped for some reason, so 0 is fully opaque and 255 is fully transparent... Why, Mojang?
-    //$$         int alpha = 255 - newColor.alpha();
-    //$$         OmniColor color = new OmniColor(red, green, blue, alpha);
-    //$$
-    //$$         OverlayTexture overlayTexture = OmniClient.get().gameRenderer.overlayTexture();
-    //$$         ((OverlayModifier) overlayTexture).setOverlayColor(color);
-    //$$     });
-    //$$ }
-    //#endif
+    public static void updateOverlayColor(PolyColor newColor) {
+        OmniClientRuntime.runOnMain(() -> {
+            int r = newColor.red();
+            int g = newColor.green();
+            int b = newColor.blue();
+            // Alpha is flipped for some reason, so 0 is fully opaque and 255 is fully transparent... Why, Mojang? Other developer note: 😭😭😭😭😭
+            int a = 255 - newColor.alpha();
+
+            OmniColor color = new OmniColor(r, g, b, a);
+            DamageTintClient.INSTANCE.LOGGER.info("updateOverlayColor called");
+
+            OverlayTexture overlayTexture = OmniClient.get().gameRenderer.overlayTexture();
+            ((OverlayModifier)overlayTexture).damageTint$setOverlayColor(color);
+        });
+    }
 }
