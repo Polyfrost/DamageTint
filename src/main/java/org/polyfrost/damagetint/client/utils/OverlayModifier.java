b0662ac089ee14a1fcea6eecab41feaf3d6b3565
Tryfle <xinaout410@gmail.com>
2026-01-15 22:06:40 -0500

    port to stonecutter

--- src/main/java/org/polyfrost/damagetint/client/utils/OverlayModifier.java
+++ src/main/java/org/polyfrost/damagetint/client/utils/OverlayModifier.java
@@ -3,5 +3,5 @@ package org.polyfrost.damagetint.client.utils;
 import dev.deftu.omnicore.api.color.OmniColor;
 
 public interface OverlayModifier {
-    void setOverlayColor(OmniColor color);
+    void damageTint$setOverlayColor(OmniColor color);
 }
